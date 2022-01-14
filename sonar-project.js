42c85c66b9d0898ce03989688585688e5bbfe6f8
Kumgan12345 <62638086+Kumgan12345@users.noreply.github.com>
2022-01-14 23:06:13 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
 	serverUrl :'http://192.168.56.102:9000',
-	token:'072506a2d74d3a200c1d1bcf9a0f3acb90197d70',
 	options : {
 		'sonar.projectName':'node-multiplayer-snake',
 		'sonar.sources': '.',
